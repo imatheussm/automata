0fed97c19431b193b42dd84c04e89b1f0fd2141d
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-05-31 09:35:34 -0300

    playground.py updated
    The changes here made reflect the change in the finiteAutomaton.__init__() method, whose functionality can now be found in the function from_txt().

--- playground.py
+++ playground.py
@@ -3,4 +3,5 @@ from sys import path as sys_path
 sys_path.append(getcwd())
 from automata import *
 
-a = finiteAutomaton("./dfa/sample.txt")
+#a = finiteAutomaton("./dfa/sample.txt")
+a = from_txt("./dfa/sample.txt")
