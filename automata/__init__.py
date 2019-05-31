e67a7d11a15e5b93aa44428f171e5e0bd36937e0
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-05-31 09:34:50 -0300

    part of finiteAutomaton.__init__() moved to __init__.py
    The part where the treatment of the strings was made was incapsulated in the from_txt() function, located under __init__.py.
    
    Further elaboration is to be made in this function; however, before that it is necessary to create the other automaton types.

--- automata/__init__.py
+++ automata/__init__.py
@@ -1,1 +1,67 @@
 from automata.finiteAutomaton import *
+
+def from_txt(automaton,verbose=False):
+	"""
+	"""
+
+	order = ["symbols", "states", "transitions", "initial_state", "final_states"]
+	properties = {item: "" for item in order}
+
+	with open(automaton,"r") as file:
+		# Opening the file and obtaining the automaton file's properties line
+		if verbose == True: print("File opened.")
+		properties_string = file.readline().strip("\n")[1:-1]
+		if verbose == True: print("Properties line: {}".format(properties_string))
+		if properties_string[0] != "{" or properties_string[-1] != "}": raise SyntaxError("The automaton properties tuple ({}) is incorrectly formatted.".format(properties_string))
+
+		# Separating the string.
+		current_property, index = 0, 0
+		if verbose == True: print("[Line 29] First property: {}".format(order[current_property]))
+		while index < len(properties_string):
+			if properties_string[index] == "{":
+				while properties_string[index] != "}":
+					properties[order[current_property]] += properties_string[index]
+					index += 1
+				properties[order[current_property]] += properties_string[index]
+				index += 1
+				current_property += 1
+				if verbose == True: print("[Line 38] properties[{}]: {}".format(order[current_property - 1],properties[order[current_property - 1]]))
+				try:
+					if verbose == True: print("[Line 40] Next property: {}".format(order[current_property]))
+					elif current_property >= len(order): raise IndexError()
+				except:
+					if verbose == True: print("[Line 43] Properties fully loaded as strings.")
+					break
+			elif properties_string[index] == "," or properties_string[index] == " ": index+=1
+			else:
+				while properties_string[index] != ",":
+					properties[order[current_property]] += properties_string[index]
+					index += 1
+				current_property+=1
+				if verbose == True: print("[Line 51] properties[{}]: {}".format(order[current_property - 1],properties[order[current_property - 1]]))
+				if verbose == True: print("[Line 52] Next property: {}".format(order[current_property]))
+		if verbose == True: print(properties)
+
+		# Separating the lists contained inside the tuple
+		for item in order:
+			if properties[item][0] == "{" and properties[item][-1] == "}": properties[item] = tuple(properties[item][1:-1].split(", "))
+
+		if verbose == True: print(properties)
+
+		# Processing the transitions
+		transitions = {}
+		transition_strings = [line.strip("\n").split(", ") for line in file.readlines()]
+		for item in transition_strings:
+			if (item[0],item[1]) not in transitions.keys(): transitions[(item[0],item[1])] = []
+			transitions[(item[0],item[1])].append(item[2])
+		if verbose == True: print(transitions)
+		for transition in transitions.keys(): transitions[transition] = tuple(transitions[transition])
+
+		properties["transitions"] = transitions
+
+		# Deleting unnecessary variables from memory
+		del(properties_string,current_property,index,transition_strings,transitions,order)
+
+		if verbose == True: print(properties)
+
+		return properties
--- automata/finiteAutomaton.py
+++ automata/finiteAutomaton.py
@@ -1,6 +1,6 @@
 class finiteAutomaton:
 	"""A generic Finite Automaton class to be inherited by the DFA, NFA and NFAE subclasses."""
-	def __init__(self,automaton,verbose=False):
+	def __init__(self,properties):
 		"""Class constructor.
 
 		Parameters
@@ -9,69 +9,12 @@ class finiteAutomaton:
 		Returns
 		-------
 		"""
-		self.tuple_order = ["symbols", "states", "transitions", "initial_state", "final_states"]
-		self.properties  = {item: "" for item in self.tuple_order}
-
-		with open(automaton,"r") as file:
-			# Opening the file and obtaining the automaton file's properties line
-			if verbose==True: print("File opened.")
-			properties_string = file.readline().strip("\n")[1:-1]
-			if verbose==True: print("Properties line: {}".format(properties_string))
-			if properties_string[0] != "{" or properties_string[-1] != "}": raise SyntaxError("The automaton properties tuple ({}) is incorrectly formatted.".format(properties_string))
-
-			# Separating the string.
-			current_property, index = 0, 0
-			if verbose==True: print("[Line 29] First property: {}".format(self.tuple_order[current_property]))
-			while index < len(properties_string):
-				if properties_string[index] == "{":
-					while properties_string[index] != "}":
-						self.properties[self.tuple_order[current_property]] += properties_string[index]
-						index += 1
-					self.properties[self.tuple_order[current_property]] += properties_string[index]
-					index += 1
-					current_property += 1
-					if verbose==True: print("[Line 38] self.properties[{}]: {}".format(self.tuple_order[current_property - 1],self.properties[self.tuple_order[current_property - 1]]))
-					try:
-						if verbose==True: print("[Line 40] Next property: {}".format(self.tuple_order[current_property]))
-						elif current_property>=len(self.tuple_order): raise IndexError()
-					except:
-						if verbose==True: print("[Line 43] Properties fully loaded as strings.")
-						break
-				elif properties_string[index] == "," or properties_string[index] == " ": index+=1
-				else:
-					while properties_string[index] != ",":
-						self.properties[self.tuple_order[current_property]] += properties_string[index]
-						index += 1
-					current_property+=1
-					if verbose==True: print("[Line 51] self.properties[{}]: {}".format(self.tuple_order[current_property - 1],self.properties[self.tuple_order[current_property - 1]]))
-					if verbose==True: print("[Line 52] Next property: {}".format(self.tuple_order[current_property]))
-			if verbose==True: print(self.properties)
-
-			# Separating the lists contained inside the tuple
-			for item in self.tuple_order:
-				if self.properties[item][0] == "{" and self.properties[item][-1] == "}": self.properties[item] = tuple(self.properties[item][1:-1].split(", "))
-
-			if verbose==True: print(self.properties)
-
-			# Processing the transitions
-			transitions = {}
-			transition_strings = [line.strip("\n").split(", ") for line in file.readlines()]
-			for item in transition_strings:
-				if (item[0],item[1]) not in transitions.keys(): transitions[(item[0],item[1])] = []
-				transitions[(item[0],item[1])].append(item[2])
-			if verbose==True: print(transitions)
-			for transition in transitions.keys(): transitions[transition] = tuple(transitions[transition])
-
-			self.properties["transitions"] = transitions
-
-			# Deleting unnecessary variables from memory
-			del(properties_string,current_property,index,transition_strings,transitions)
-
-			if verbose==True: print(self.properties)
+		self.properties  = properties
 
 	def __repr__(self):
-		return "<finiteAutomaton object>\n      Symbols: {}\n       States: {}\n\
-  Transitions: {}\nInitial State: {}\n Final States: {}".format(", ".join(self.properties["symbols"]),
+		return "<{} object>\n      Symbols: {}\n       States: {}\n\
+  Transitions: {}\nInitial State: {}\n Final States: {}".format(str(type(self)).split("'")[1],
+																", ".join(self.properties["symbols"]),
 																", ".join(self.properties["states"]),
 																str(self.properties["transitions"]),
 																self.properties["initial_state"],
