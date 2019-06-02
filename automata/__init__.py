4ed48113e1433b4ea81fcec6bcd422ffef45d167
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-06-02 15:16:05 -0300

    Encoding added, line numbers fixed in print statements
    Now, the ε symbol is properly loaded. In addition, the line numbers, which were outdated, are now matching the lines they are actually on.

--- automata/__init__.py
+++ automata/__init__.py
@@ -27,16 +27,16 @@ def from_txt(automaton,verbose=False):
 	order = ["symbols", "states", "transitions", "initial_state", "final_states"]
 	properties = {item: "" for item in order}
 
-	with open(automaton,"r") as file:
+	with open(automaton,"r",encoding="utf-8") as file:
 		# Opening the file and obtaining the automaton file's properties line
-		if verbose == True: print("File opened.")
+		if verbose == True: print("[LINE 32] File opened.")
 		properties_string = file.readline().strip("\n")[1:-1]
-		if verbose == True: print("Properties line: {}".format(properties_string))
+		if verbose == True: print("[LINE 34] Properties line: {}".format(properties_string))
 		if properties_string[0] != "{" or properties_string[-1] != "}": raise SyntaxError("The automaton properties tuple ({}) is incorrectly formatted.".format(properties_string))
 
 		# Separating the string.
 		current_property, index = 0, 0
-		if verbose == True: print("[Line 29] First property: {}".format(order[current_property]))
+		if verbose == True: print("[Line 39] First property: {}".format(order[current_property]))
 		while index < len(properties_string):
 			if properties_string[index] == "{":
 				while properties_string[index] != "}":
@@ -45,12 +45,12 @@ def from_txt(automaton,verbose=False):
 				properties[order[current_property]] += properties_string[index]
 				index += 1
 				current_property += 1
-				if verbose == True: print("[Line 38] properties[{}]: {}".format(order[current_property - 1],properties[order[current_property - 1]]))
+				if verbose == True: print("[Line 48] properties[{}]: {}".format(order[current_property - 1],properties[order[current_property - 1]]))
 				try:
-					if verbose == True: print("[Line 40] Next property: {}".format(order[current_property]))
+					if verbose == True: print("[Line 50] Next property: {}".format(order[current_property]))
 					elif current_property >= len(order): raise IndexError()
 				except:
-					if verbose == True: print("[Line 43] Properties fully loaded as strings.")
+					if verbose == True: print("[Line 53] Properties fully loaded as strings.")
 					break
 			elif properties_string[index] == "," or properties_string[index] == " ": index+=1
 			else:
@@ -58,15 +58,15 @@ def from_txt(automaton,verbose=False):
 					properties[order[current_property]] += properties_string[index]
 					index += 1
 				current_property+=1
-				if verbose == True: print("[Line 51] properties[{}]: {}".format(order[current_property - 1],properties[order[current_property - 1]]))
-				if verbose == True: print("[Line 52] Next property: {}".format(order[current_property]))
+				if verbose == True: print("[Line 61] properties[{}]: {}".format(order[current_property - 1],properties[order[current_property - 1]]))
+				if verbose == True: print("[Line 62] Next property: {}".format(order[current_property]))
 		if verbose == True: print(properties)
 
 		# Separating the lists contained inside the tuple
 		for item in order:
 			if properties[item][0] == "{" and properties[item][-1] == "}": properties[item] = tuple(properties[item][1:-1].split(", "))
 
-		if verbose == True: print(properties)
+		if verbose == True: print("[LINE 69] {}".format(properties))
 
 		# Processing the transitions
 		transitions = {}
@@ -74,7 +74,7 @@ def from_txt(automaton,verbose=False):
 		for item in transition_strings:
 			if (item[0],item[1]) not in transitions.keys(): transitions[(item[0],item[1])] = []
 			transitions[(item[0],item[1])] += [i.strip("{").strip("}") for i in item[2:]]
-		if verbose == True: print(transitions)
+		if verbose == True: print("[LINE 77] {}".format(transitions))
 		for transition in transitions.keys(): transitions[transition] = tuple(transitions[transition])
 
 		properties["transitions"] = transitions
@@ -82,6 +82,6 @@ def from_txt(automaton,verbose=False):
 		# Deleting unnecessary variables from memory
 		del(properties_string,current_property,index,transition_strings,transitions,order)
 
-		if verbose == True: print(properties)
+		if verbose == True: print("[LINE 85] {}".format(properties))
 
 		return properties
