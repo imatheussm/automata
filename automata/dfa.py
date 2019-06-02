8427844a43e5d4fcc47619c8abe25edac8d1de7c
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-06-02 11:51:33 -0300

    Documentation updates
    Small stuff, as always.

--- automata/dfa.py
+++ automata/dfa.py
@@ -16,7 +16,7 @@ class deterministicFiniteAutomaton(finiteAutomaton):
 			Example: {'symbols': ('0', '1'), 'states': ('q0', 'q1'), 'transitions': {('q0', '1'): ('q0',), ('q0', '0'): ('q1',), ('q1', '0'): ('q0',), ('q1', '1'): ('q1',)}, 'initial_state': 'q0', 'final_states': ('q0',)}
 
 			An exception will be raised if the automaton doesn't meet the requirements to be deterministic.
-			If you have a .txt file containing the automaton's properties and you want to convert it, use the from_txt() function instead.
+			If you have a .txt file containing the automaton's properties and you want to convert it into a finiteAutomaton object, use the from_txt() function instead.
 
 		Returns
 		-------
