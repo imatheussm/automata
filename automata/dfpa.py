9005966428d468ab78221d45428b321ed8383ca8
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-06-04 13:35:11 -0300

    dfpa.py created
    It works flawlessly! In addition, it already has the new format of TypeError exception recently adopted to the remaining automaton types.
    
    I have to incorporate into from_txt() the ability to return the object directly, instead of just returning the properties. I'll probably set up an attribute to allow me to decide among the two options and set the newer one (returning the object) as the default one.

---
+++ automata/dfpa.py
@@ -0,0 +1,26 @@
+﻿from automata.fa import finiteAutomaton
+
+class deterministicFinitePushdownAutomaton(finiteAutomaton):
+	"""A deterministic, finite automaton which employs a stack."""
+	def __init__(self,properties):
+		"""Class constructor.
+
+		Parameters
+		----------
+		self : deterministicFinitePushdownAutomaton
+			A deterministicFinitePushdownAutomaton object.
+		properties : dict, str(dict)
+			The properties of said automaton. It must be a dictionary object or its string representation.
+
+			Example: {'symbols': ('0', '1'), 'states': ('q0', 'q1'), 'transitions': {('q0', '1'): ('q0',), ('q0', '0'): ('q1',), ('q1', '0'): ('q0',), ('q1', '1'): ('q1',)}, 'initial_state': 'q0', 'final_states': ('q0',)}
+
+			An exception will be raised if the automaton doesn't meet the requirements to be deterministic.
+			If you have a .txt file containing the automaton's properties and you want to convert it into a finiteAutomaton object, use the from_txt() function instead.
+
+		Returns
+		-------
+		deterministicFinitePushdownAutomaton
+			A deterministicFinitePushdownAutomaton object with the stipulated properties.
+		"""
+		super().__init__(properties)
+		if "stack_symbols" not in self.properties.keys(): raise TypeError("The automaton provided does not have a stack.")
