bd565e0195e179c1bf6d5ae9425d1d5424cb40d1
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-06-04 21:52:47 -0300

    .process_word() created; .transitions() improved
    .process_word() was created, based upon the implementation seen under the finiteAutomaton class, but considering the particularities of this automaton.
    
    In addition, .transitions() got improved to contemplate the existance of empty stack moves (it doesn't ready anything from the stack), in addition to the "?" verification symbol previously contemplated.

--- automata/fpa.py
+++ automata/fpa.py
@@ -97,6 +97,68 @@ class finitePushdownAutomaton(finiteAutomaton):
 		try: return self.properties["transitions"][(current_state,symbol,stack_symbol)]
 		except: return None
 
+	def process_word(self,word,current_states=None,verbose=False):
+		"""Checks if a word can be processed by the finiteAutomaton object.
+
+		Parameters
+		----------
+		self : finiteAutomaton
+			An object of the class finiteAutomaton (or any of its subclasses).
+		word : str
+			The word to be processed.
+		current_states : tuple(str), str, NoneType (default = None)
+			The state to be processed. It is actively used throughout the function, since it is recursive.
+		verbose : bool
+			Serves to tell the function if print statements should be displayed as the word is processed.
+
+		Returns
+		-------
+		bool
+			The result of the processing. In other words: if the word has been accepted by the automaton or not.
+		"""
+		is_final = False
+
+		if current_states == None: current_states = (self.properties["initial_state"],)
+		if verbose == True: print("[LINE 102] Current state (s): {}".format(", ".join(current_states)))
+		if word[0] not in self.properties["symbols"]:
+			if verbose == True: print("\n[LINE 163] The symbol {} is not contained in this automaton's alphabet. As such, it cannot be processed.\n\
+[LINE 164] The function returns ".format(word[0]),
+										 end="")
+			return False
+		new_current_states = []
+		for current_state in current_states:
+			if (current_state, "ε") in self.properties["transitions"].keys():
+				if verbose == True:
+					print("[LINE 171] This state has the empty transition. All possibilities shall be tested.")
+					print("[LINE 172] Executing δ({{{}}}, ε) and δ({}, {})".format(current_state,"{" + "".join(str(self.process_symbol(current_state,"ε"))[1:-1].strip(",").split("'")) + "}",word[0]))
+				try: new_current_states += list(self.process_symbol(self.process_symbol(current_state,"ε"),word[0]))
+				except: pass
+				if verbose == True:
+					print("[LINE 175] Executing δ({{{}}}, {}) and δ({}, ε)".format(current_state,word[0],"{" + "".join(str(self.process_symbol(current_state,word[0]))[1:-1].strip(",").split("'")) + "}"))
+				try: new_current_states += list(self.process_symbol(self.process_symbol(current_state,word[0]),"ε"))
+				except: pass
+			if verbose == True: print("[LINE 178] Executing δ({{{}}}, {})\n".format(current_state,word[0]))
+			try: new_current_states += list(self.process_symbol(current_state,word[0]))
+			except: pass
+		current_states = tuple(sorted(set(new_current_states)))
+		del(new_current_states)
+		if len(word) > 1:
+			if verbose == True: print("[LINE 184] Remaining symbols to be processed: {}\n".format(word[1:]))
+			return self.process_word(word[1:],current_states=current_states,verbose=verbose)
+		else:
+			if verbose == True: print("\n[LINE 187] Final states found: {}\n".format(", ".join(current_states)))
+			for state in current_states:
+				if state in self.properties["final_states"]:
+					if verbose == True: print("[LINE 190] {} is final.".format(state))
+					is_final = True
+				else:
+					if verbose == True: print("[LINE 193] {} is not final.".format(state))
+			if is_final == True:
+				if verbose == True: print("\n[LINE 195] The word has been accepted. The function returns ",end="")
+			else:
+				if verbose == True: print("\n[LINE 197] The word has not been accepted. The function returns ",end="")
+			return is_final
+
 	def transitions(self,to_str=False,body_left_margin=0):
 		"""Prints the transitions in a transition table format.
 
@@ -114,16 +176,16 @@ class finitePushdownAutomaton(finiteAutomaton):
 			Returns a string if to_str == True; otherwise, it just prints it and doesn't return anything.
 		"""
 		string = []
-		column_titles, space = ["δ"] + list(self.properties["symbols"]) + ["?"], max([len(item) for item in self.properties["transitions"].values()]) * 4 + 2
+		column_titles, stack_symbols, space = ["δ"] + list(self.properties["symbols"]) + ["?"], list(self.properties["stack_symbols"]) + ["?"], max([len(item) for item in self.properties["transitions"].values()]) * 4 + 2
 		column_titles[0] = int((space) / 2) * " " + column_titles[0] + int((space) / 2) * " "
 		for (origins, destinations) in self.properties["transitions"].items():
 			print(origins)
-			if "ε" in origins:
-				column_titles.append("ε")
-				break
+			if "ε" in origins[:-1]: column_titles.append("ε")
+			elif "ε" in origins[-1]: stack_symbols.append("ε")
+
 		string.append(str("|".join(["{1:^{0}}".format(space,element) for element in column_titles + ["Γ"]])))
 		i, line, previous_state = 1, [], None
-		for (state, stack_symbol, symbol) in list(product(self.properties["states"],list(self.properties["stack_symbols"]) + ["?"],column_titles[1:])):
+		for (state, stack_symbol, symbol) in list(product(self.properties["states"],stack_symbols,column_titles[1:])):
 			if line == []:
 				if state != previous_state: previous_state, line = state, ["{0}{2:^{1}}".format(body_left_margin * " ",space,state)]
 				else: line = ["{0}{2:^{1}}".format(body_left_margin * " ",space," ")]
